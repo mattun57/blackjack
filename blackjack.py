6d3fa2378797ca8e940e75c7b7f5a1d58f42ca13
ryusei <sthglobe12@gmail.com>
2019-06-22 16:11:23 +0900

    指定のあった箇所を修正しました。

--- blackjack.py
+++ blackjack.py
@@ -6,15 +6,21 @@
 # J-Kが10、Aは1または11として、2枚の数字の合計が多い方が勝ち
 # ユーザーは手札を何回でも引ける
 # ディーラーの手札は最後にわかる
-# 21ちょうどで買った場合は「BlackJack!を表示」
+# 21ちょうどで勝った場合は「BlackJack!を表示」
+# Aが1か11かは、最後の点数計算の時に適切なものを自動的に計算する
 
 import random
 
-# A = 11
 J = 10
 Q = 10
 K = 10
 cards = ["A",2,3,4,5,6,7,8,9,10,J,Q,K] * 4
+
+# ユーザーの手札まとめ
+you_hands = []
+
+# ディーラーの手札まとめ
+dealer_hands = []
 
 
 def main():
@@ -24,51 +30,51 @@ def main():
     you_bet = input("いくら賭けますか？ 金額を入力してください:")
     print("あなたの賭けた金額 $" + you_bet)
 
-    you_hand1 = cards.pop(get_rand_index(cards))
-    you_hand2 = cards.pop(get_rand_index(cards))
-    print("あたなの手札" + str(you_hand1),str(you_hand2))
+    you_hands.append(cards.pop(get_rand_index(cards)))
+    you_hands.append(cards.pop(get_rand_index(cards)))
+    print("あたなの手札" + str(you_hands))
 
-    dealer_hand1 = cards.pop(get_rand_index(cards))
-    if dealer_hand1 == "A":
-        dealer_hand1 = "11"
-    dealer_hand2 = cards.pop(get_rand_index(cards))
-    if dealer_hand2 == "A":
-        dealer_hand2 = "11"
 
-    # Aを1か11か選ぶ（1回目)
-    if you_hand1 == "A":
-        A_choice = input("Aの数字はどうしますか？ 1 or 11 :")
-        if A_choice == "1":
-            you_hand1 = "1"
-        elif A_choice == "11":
-            you_hand1 = "11"
-    
-    if you_hand2 == "A":
-        A_choice = input("Aの数字はどうしますか？ 1 or 11 :")
-        if A_choice == "1":
-            you_hand2 = "1"
-        elif A_choice == "11":
-            you_hand2 = "11"
+    # ディーラーは自分の手持ちのカードの合計が「17」以上になるまでヒットし続ける。
+    dealer_total = 0
+    while dealer_total <= 17:
+        dealer_hands.append(cards.pop(get_rand_index(cards)))
+        if "A" in dealer_hands:
+           dealer_hands.remove("A")
+           dealer_hands.append(11)
+        dealer_total = sum(dealer_hands)
 
-    you_add = 0
-    add = input("カードを追加しますか？ 1-HIT, 2-STAND :")
-    if int(add) == 1 :
-        you_add = random.choice(cards)
-        print("追加されたカード" + str(you_add))
 
-    # Aを1か11か選ぶ（2回目)
-    if you_add == "A":
-        A_choice = input("Aの数字はどうしますか？ 1 or 11 :")
-        if A_choice == "1":
-            you_add = "1"
-        elif A_choice == "11":
-            you_add = "11"
+    # ユーザーは手札を見ながら何度でもヒットできる
+    add = "1"
+    while add == "1":
+        add = input("カードを追加しますか？ 1-HIT, 2-STAND :")
+        if add == "1":
+            you_hands.append(cards.pop(get_rand_index(cards)))
+        print(you_hands)
 
-    print("ディーラーの手札" + str(dealer_hand1),str(dealer_hand2))
 
-    you_total = int(you_hand1) + int(you_hand2) + int(you_add)
-    dealer_total = int(dealer_hand1) + int(dealer_hand2)
+    print("ディーラーの手札" + str(dealer_hands))
 
+    # Aが1か11か適切なものを自動的に計算
+      # 文字列"A"を取り除く
+    if "A" in you_hands:
+        A_num = you_hands.count("A")
+        for i in range(int(A_num)):
+            you_hands.remove("A")
+        # 文字列"A"の数分 1 か 11 を追加する
+        for j in range(int(A_num)):
+            if sum(you_hands) >= 11:
+                you_hands.append(1)
+            else:
+                you_hands.append(11)
+
+
+    # ディーラーとユーザーの手札の合計
+    you_total = sum(you_hands)
+    dealer_total = sum(dealer_hands)
+
+    # 勝敗の判定と出力
     if you_total > dealer_total:
         if you_total <= 21:
             if you_total == 21:
