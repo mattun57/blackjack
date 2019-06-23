57ab2cd4ae2ec9cf243a55a0ea5b9630b72fdfd7
ryusei <sthglobe12@gmail.com>
2019-06-23 09:57:06 +0900

    update 3.2

--- blackjack.py
+++ blackjack.py
@@ -16,88 +16,115 @@ Q = 10
 K = 10
 cards = ["A",2,3,4,5,6,7,8,9,10,J,Q,K] * 4
 
-# ユーザーの手札まとめ
-you_hands = []
-
-# ディーラーの手札まとめ
-dealer_hands = []
-
 
 def main():
     """
     メイン関数
     """
-    you_bet = input("いくら賭けますか？ 金額を入力してください:")
-    print("あなたの賭けた金額 $" + you_bet)
+    # 手持ちの財産
+    you_money = 10000
 
-    you_hands.append(cards.pop(get_rand_index(cards)))
-    you_hands.append(cards.pop(get_rand_index(cards)))
-    print("あたなの手札" + str(you_hands))
+    # ユーザーがゲームをやめるを選択するまで続くようにする
+    game_play = "1"
+    while game_play == "1":
+
+        # ユーザーの手札まとめ
+        you_hands = []
+
+        # ディーラーの手札まとめ
+        dealer_hands = []
+
+        print("所持金 $" + str(you_money))
+        you_bet = input("いくら賭けますか？金額を入力してください:")
+
+        # 所持金以上賭けようとしたら弾く
+        if you_money < int(you_bet):
+            print("お引き取りください")
+            break
+
+        print("あなたの賭けた金額 $" + you_bet)
+
+        you_hands.append(cards.pop(get_rand_index(cards)))
+        you_hands.append(cards.pop(get_rand_index(cards)))
+        print("あたなの手札" + str(you_hands))
 
 
-    # ディーラーは自分の手持ちのカードの合計が「17」以上になるまでヒットし続ける。
-    dealer_total = 0
-    while dealer_total <= 17:
-        dealer_hands.append(cards.pop(get_rand_index(cards)))
-        if "A" in dealer_hands:
-           dealer_hands.remove("A")
-           dealer_hands.append(11)
+        # ディーラーは自分の手持ちのカードの合計が「17」以上になるまでヒットし続ける。
+        dealer_total = 0
+        while dealer_total <= 17:
+            dealer_hands.append(cards.pop(get_rand_index(cards)))
+            if "A" in dealer_hands:
+                dealer_hands.remove("A")
+                dealer_hands.append(11)
+            dealer_total = sum(dealer_hands)
+
+
+        # ユーザーは手札を見ながら何度でもヒットできる
+        add = "1"
+        while add == "1":
+            add = input("カードを追加しますか？ 1-HIT, 2-STAND :")
+            if add == "1":
+                you_hands.append(cards.pop(get_rand_index(cards)))
+            print(you_hands)
+
+
+        print("ディーラーの手札" + str(dealer_hands))
+
+        # Aが1か11か適切なものを自動的に計算
+        # 文字列"A"を取り除く
+        if "A" in you_hands:
+            A_num = you_hands.count("A")
+            for i in range(int(A_num)):
+                you_hands.remove("A")
+            # 文字列"A"の数分 1 か 11 を追加する
+            for j in range(int(A_num)):
+                if sum(you_hands) >= 11:
+                    you_hands.append(1)
+                else:
+                    you_hands.append(11)
+
+
+        # ディーラーとユーザーの手札の合計
+        you_total = sum(you_hands)
         dealer_total = sum(dealer_hands)
 
+        # 勝敗の判定と出力
+        if you_total > dealer_total:
+            if you_total <= 21:
+                if you_total == 21:
+                    print("Black Jack!")
+                    bet = int(you_bet) * 2
+                    print("$" + str(bet) + " GET!")
+                    you_money -= int(you_bet)
+                    you_money += bet
+                else:
+                    print("YOU WIN!")
+                    bet = int(you_bet) * 2
+                    print("$" + str(bet) + " GET!")
+                    you_money -= int(you_bet)
+                    you_money += bet
 
-    # ユーザーは手札を見ながら何度でもヒットできる
-    add = "1"
-    while add == "1":
-        add = input("カードを追加しますか？ 1-HIT, 2-STAND :")
-        if add == "1":
-            you_hands.append(cards.pop(get_rand_index(cards)))
-        print(you_hands)
 
-
-    print("ディーラーの手札" + str(dealer_hands))
-
-    # Aが1か11か適切なものを自動的に計算
-      # 文字列"A"を取り除く
-    if "A" in you_hands:
-        A_num = you_hands.count("A")
-        for i in range(int(A_num)):
-            you_hands.remove("A")
-        # 文字列"A"の数分 1 か 11 を追加する
-        for j in range(int(A_num)):
-            if sum(you_hands) >= 11:
-                you_hands.append(1)
             else:
-                you_hands.append(11)
-
-
-    # ディーラーとユーザーの手札の合計
-    you_total = sum(you_hands)
-    dealer_total = sum(dealer_hands)
-
-    # 勝敗の判定と出力
-    if you_total > dealer_total:
-        if you_total <= 21:
-            if you_total == 21:
-                print("Black Jack!")
+                print("YOU LOSE!")
+                you_money -= int(you_bet)
+        elif you_total == dealer_total:
+            print("DRAW")
+            print("払い戻し金 $" + you_bet)
+        else:
+            if dealer_total <= 21:
+                print("YOU LOSE!")
+                you_money -= int(you_bet)
+            else:
+                print("YOU WIN!")
                 bet = int(you_bet) * 2
                 print("$" + str(bet) + " GET!")
-            print("YOU WIN!")
-            bet = int(you_bet) * 2
-            print("$" + str(bet) + " GET!")
+                you_money -= int(you_bet)
+                you_money += bet
+        
+        # ユーザーがゲームを続けるか選ぶ
+        game_play = input("ゲームを続けますか？ はい-1, いいえ-2 :")
 
-
-        else:
-            print("YOU LOSE!")
-    elif you_total == dealer_total:
-        print("DRAW")
-        print("払い戻し金 $" + you_bet)
-    else:
-        if dealer_total <= 21:
-            print("YOU LOSE!")
-        else:
-            print("YOU WIN!")
-            bet = int(you_bet) * 2
-            print("$" + str(bet) + " GET!")
 
 def get_rand_index(cards):
     """
